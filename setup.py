978499ba654d81fc9968565709987b7cbede53cf
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2021-06-08 18:27:30 +0530

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/vyoma_download',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
